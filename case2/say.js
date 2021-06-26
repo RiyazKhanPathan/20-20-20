b4ad77e35dd1afc594091d290dcebf60031215b6
Riyaz Khan Pathan <66507064+RiyazKhanPathan@users.noreply.github.com>
2021-06-26 11:34:40 +0530

    TextToSpeech file

--- case2/say.js
+++ case2/say.js
@@ -2,7 +2,7 @@ module.exports = (text)=>{
     var proc = require('child_process');
     var commands = [ 'Add-Type -AssemblyName System.speech; $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer; $speak.Speak([Console]::In.ReadToEnd())' ];
     var options = { shell: true };
-    var childD = proc.spawn('powershell', commands, options);
-    childD.stdin.end(text);
+    var childProc = proc.spawn('powershell', commands, options);
+    childProc.stdin.end(text);
     
 }
